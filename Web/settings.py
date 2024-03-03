330b05e04fe252b0bbbc00569bd0bc0113dfef6b
J. Japa <jjv@live.dk>
2024-03-03 20:38:23 +0100

    Trying SQLLite

--- Web/settings.py
+++ Web/settings.py
@@ -98,19 +98,19 @@ WSGI_APPLICATION = 'Web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ciba',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ciba',
+#     }
+# }
 
 
 # Password validation
