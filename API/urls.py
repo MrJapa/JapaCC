5974bd1933ffd1db3493f306d87cdc76c0af8565
J. Japa <jjv@redmark.dk>
2024-03-04 23:00:36 +0100

    Finally works...

--- API/urls.py
+++ API/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import NyBestillingUpdateAPIView
+from .views import *
+
+# urlpatterns = [
+#     path('api/orders/', NyBestillingUpdateAPIView.as_view(), name='order-create'),
+#     path('orders/<int:pk>/', NyBestillingUpdateAPIView.as_view(), name='order-update'),
+    
+# ]
 
 urlpatterns = [
-    path('nybestilling/<int:pk>/', NyBestillingUpdateAPIView.as_view(), name='order-update'),
-    
+    path('orders/', create_order),
+    path('api/orders/<int:pk>/', take_order, name='order-update'),
 ]
